7649939a12e498c0ee2c458332282d14d7b00ab4
jhmt <jhashimot00@gmail.com>
2016-07-23 15:37:41 +1200

    Added Contact Us page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Routes, ROUTER_DIRECTIVES} from '@angular/router';
+import {ContactComponent} from './contact/contact.component';
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
 
@@ -12,6 +13,7 @@ import {HomeComponent} from './home/home.component';
   {path: '/',            component: HomeComponent },
   {path: '/home',        component: HomeComponent },
   {path: '/about',       component: AboutComponent },
+  {path: '/contact',     component: ContactComponent },
   {path: '/*',           component: HomeComponent }
 ])
 export class AppComponent {}
